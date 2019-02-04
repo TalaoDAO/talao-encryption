8e1b2b66b1f09305e050e8be864c18609807d9dc
Guillaume Duveau <guillaume.duveau@gmail.com>
2019-02-04 21:08:48 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,100 +1,104 @@
-const crypto = require('crypto');
-const NodeRSA = require('node-rsa');
-const aesjs = require('aes-js');
-const web3 = require('web3');
+const crypto = require('crypto')
+const NodeRSA = require('node-rsa')
+const aesjs = require('aes-js')
+const web3 = require('web3')
 
 class TalaoEncryption {
 
   constructor() {
-    this.rsa = null;
-    this.aes = null;
+    this.rsa = null
+    this.aes = null
   }
 
   generateRsa() {
-    this.rsa = new NodeRSA({b: 2048});
-    return this.rsa;
+    this.rsa = new NodeRSA({b: 2048})
+    return this.rsa
   }
 
   loadRsa(pem) {
-    this.rsa = new NodeRSA(pem);
-    return this.rsa;
+    this.rsa = new NodeRSA(pem)
+    return this.rsa
   }
 
   getRsa() {
-    return this.rsa;
+    return this.rsa
   }
 
   getPrivateRsa() {
-    return this.rsa.exportKey('private');
+    return this.rsa.exportKey('private')
   }
 
   getPublicRsa() {
-    return this.rsa.exportKey('public');
+    return this.rsa.exportKey('public')
   }
 
   getPublicRsaForEthereum() {
-    return web3.utils.asciiToHex(this.getPublicRsa());
+    return web3.utils.asciiToHex(this.getPublicRsa())
   }
 
   generateAes() {
-    this.aes = crypto.randomFillSync(Buffer.alloc(16));
-    return this.aes;
+    this.aes = crypto.randomFillSync(Buffer.alloc(16))
+    return this.aes
   }
 
   loadAes(hex) {
-    this.aes = Buffer.from(hex, 'hex');
-    return this.aes;
+    this.aes = Buffer.from(hex, 'hex')
+    return this.aes
   }
 
   getAes() {
-    return this.aes;
+    return this.aes
   }
 
   getAesHex() {
-    return this.aes.toString('hex');
+    return this.aes.toString('hex')
+  }
+
+  rsaEncryptForEthereum(_clear) {
+    const encrypted = this.rsa.encrypt(_clear)
+    const encryptedHex = encrypted.toString('hex')
+    return '0x' + encryptedHex
   }
 
   rsaEncryptAesForEthereum() {
-    const encrypted = this.rsa.encrypt(this.getAes());
-    const encryptedHex = encrypted.toString('hex');
-    return '0x' + encryptedHex;
+    return this.rsaEncryptForEthereum(this.getAes())
   }
 
   rsaDecryptAesFromEthereum(ethereum) {
-    const encrypted = ethereum.substr(2);
-    const buffer = Buffer.from(encrypted, 'hex');
-    return this.rsa.decrypt(buffer);
+    const encrypted = ethereum.substr(2)
+    const buffer = Buffer.from(encrypted, 'hex')
+    return this.rsa.decrypt(buffer)
   }
 
   rsaDecryptAesFromEthereumAndLoad(ethereum) {
-    const decrypted = this.rsaDecryptAesFromEthereum(ethereum);
-    this.loadAes(decrypted);
+    const decrypted = this.rsaDecryptAesFromEthereum(ethereum)
+    this.loadAes(decrypted)
   }
 
   aesEncrypt(text) {
-    const bytes = aesjs.utils.utf8.toBytes(text);
-    const ctr = new aesjs.ModeOfOperation.ctr(this.getAes(), new aesjs.Counter(5));
-    const encryptedBytes = ctr.encrypt(bytes);
-    const encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
-    return encryptedHex;
+    const bytes = aesjs.utils.utf8.toBytes(text)
+    const ctr = new aesjs.ModeOfOperation.ctr(this.getAes(), new aesjs.Counter(5))
+    const encryptedBytes = ctr.encrypt(bytes)
+    const encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes)
+    return encryptedHex
   }
 
   aesDecrypt(encryptedHex) {
-    const encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex);
-    const ctr = new aesjs.ModeOfOperation.ctr(this.getAes(), new aesjs.Counter(5));
-    const decryptedBytes = ctr.decrypt(encryptedBytes);
-    const decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes);
-    return decryptedText;
+    const encryptedBytes = aesjs.utils.hex.toBytes(encryptedHex)
+    const ctr = new aesjs.ModeOfOperation.ctr(this.getAes(), new aesjs.Counter(5))
+    const decryptedBytes = ctr.decrypt(encryptedBytes)
+    const decryptedText = aesjs.utils.utf8.fromBytes(decryptedBytes)
+    return decryptedText
   }
 
   aesEncryptForEthereum(text) {
-    return '0x' + this.aesEncrypt(text);
+    return '0x' + this.aesEncrypt(text)
   }
 
   aesDecryptFromEthereum(ethereum) {
-    return this.aesDecrypt(ethereum.substr(2));
+    return this.aesDecrypt(ethereum.substr(2))
   }
 
 }
 
-module.exports = TalaoEncryption;
+module.exports = TalaoEncryption
